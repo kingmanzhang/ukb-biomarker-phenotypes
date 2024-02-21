d94c4041e519e0bc34720581bab9512ce543ccfc
Aaron Zhang <xingmin.zhang@regeneron.com>
2024-02-21 11:27:52 -0500

    add code to combine all the biomarkers

---
+++ combine.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+
+filenames <- list.files('.')
+
+filenames_csv <- filenames[str_detect(filenames, '.csv$')]
+
+biomarkernames <- str_remove(filenames_csv, '.csv')
+
+read_content <- function(filename, biomarkername){
+  df <- read_csv(filename)
+  df$biomarker <- biomarkername
+  return (df)
+}
+
+df <- read_content('Albumin.csv', 'Albumin')
+
+contents <- map2(filenames_csv, biomarkernames, read_content)
+
+contents <- reduce(contents, bind_rows)
+
+contents <- contents %>% 
+  relocate(biomarker, .before = everything())
+
+write_csv(contents, file = 'combined.csv')
